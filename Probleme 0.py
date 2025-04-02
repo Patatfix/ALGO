df1f110844201a3ba220256b4af030b81e31dd6f
Adrien Candela <adrien.candela@gmail.com>
2025-04-02 14:09:24 +0200

    Update Probleme 0.py

--- Probleme 0.py
+++ Probleme 0.py
@@ -2,11 +2,11 @@
 """
 Created on Wed Apr  2 13:22:52 2025
 
-@author: Noreddine
+@author: Adrien
 """
 
 def compatible(creneau_1, creneau_2):
-    if (j[0] <= dim[0] and j[0] <= dim[1]) or (j[1] <= dim[0] and j[1] <= dim[1]):
+    if (creneau_1[0] <= creneau_2[0] and creneau_1[0] <= creneau_2[1]) or (creneau_1[1] <= creneau_2[0] and creneau_1[1] <= creneau_2[1]):
         return False
     return True
 
@@ -15,3 +15,4 @@ def compatible_planning(planning, creneau):
         if not compatible(j, creneau):
             return False
     return True
+
