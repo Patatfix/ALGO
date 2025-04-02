9fa48151d3dbda8f797a8e1dda40a6c0348649c1
Adrien Candela <adrien.candela@gmail.com>
2025-04-02 13:58:23 +0200

    Update Probleme 0.py

--- Probleme 0.py
+++ Probleme 0.py
@@ -5,13 +5,7 @@ Created on Wed Apr  2 13:22:52 2025
 @author: Noreddine
 """
 
-def enumeration_exhaustive(demandes, lenSol=0):
-    N = len(demandes)
-    Solution = []
-    for i in range(N):
-        dim = dimension.pop(i)
-        Solution.append(dim)
-        enumeration_exhaustive(demandes, lenSol + 1)
-    
-        dim.insert(0, demandes[i])
-    return Solution
+def compatible(creneau_1, creneau_2):
+    if (j[0] <= dim[0] and j[0] <= dim[1]) or (j[1] <= dim[0] and j[1] <= dim[1]):
+        return False
+    return True
