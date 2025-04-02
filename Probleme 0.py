6754a0f3614672720085d11759a743c2c99de5aa
warsasat <warsasat1104@gmail.com>
2025-04-02 13:46:10 +0200

    Update Probleme 0.py

--- Probleme 0.py
+++ Probleme 0.py
@@ -6,4 +6,8 @@ Created on Wed Apr  2 13:22:52 2025
 """
 
 def optim_planning(demandes , nbr_jour = 0):
+    if len(demandes) == 0:
+        return 
+    for i in range(len(demandes)):
+        if optim_demande
     
