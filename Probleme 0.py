1e1bff044a3c70471933d4219734bc030093f9e8
BOTET Gaël <gaelbotet@gmail.com>
2025-04-02 13:56:37 +0200

    Update Probleme 0.py

--- Probleme 0.py
+++ Probleme 0.py
@@ -1,8 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr  2 13:22:52 2025
+def optim_planning_v0(demandes, rdv = (0,0)):
+    h0, hmax = 0, 10**9
+    n = len(demandes)
+    demandes_classées = [[demande[i], i] for i in range(n)] #on ajoute un indice pour pouvoir ranger a la fin dans l'ordre original
+    demandes_classées = sorted(demandes_classées, key = lambda x: x[0][0]) #on trie par heure de début de rdv
 
-@author: Noreddine
-"""
+    
+    
+    
 
-caca vv opjuy
+    
+    
+    demandes_classées = sorted(demandes_classées, key = lambda x: x[1])  #on remet dans l'ordre original  
+    return (planning) #on renvoie une liste de doublet (liste de rdv)
