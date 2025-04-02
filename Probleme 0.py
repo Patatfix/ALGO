30ddb5223f5bb4663c0c394417d5ebfd403d3e4a
warsasat <warsasat1104@gmail.com>
2025-04-02 13:55:00 +0200

    Update Probleme 0.py

--- Probleme 0.py
+++ Probleme 0.py
@@ -5,9 +5,15 @@ Created on Wed Apr  2 13:22:52 2025
 @author: Romain
 """
 
-def optim_planning(demandes , nbr_jour = 0):
+Hmax = 0
+
+def est_compatible(creneaux):
+    for i in range(len(creneaux)):
+        for j in range(len(creneaux)):
+
+def optim_planning(demandes , nbr_jour = 0, i = 0):
     if len(demandes) == 0:
         return 
-    for i in range(len(demandes)):
-        if optim_demande
+    for j in range(i,len(demandes)):
+        if demandes[]
     
