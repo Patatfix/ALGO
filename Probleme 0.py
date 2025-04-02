3936c95d4c99506ca904babb60207e8194314a62
Adrien Candela <adrien.candela@gmail.com>
2025-04-02 13:52:15 +0200

    Update Probleme 0.py

--- Probleme 0.py
+++ Probleme 0.py
@@ -5,4 +5,13 @@ Created on Wed Apr  2 13:22:52 2025
 @author: Noreddine
 """
 
-caca
+def enumeration_exhaustive(demandes, lenSol=0):
+    N = len(demandes)
+    Solution = []
+    for i in range(N):
+        dim = dimension.pop(i)
+        Solution.append(dim)
+        enumeration_exhaustive(demandes, lenSol + 1)
+    
+        dim.insert(0, demandes[i])
+    return Solution
