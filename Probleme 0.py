349835952e7025b9a1edf102b71197ff6b312c78
scorp-dotcom <remygaudin38@gmail.com>
2025-04-02 13:45:50 +0200

    Update Probleme 0.py

--- Probleme 0.py
+++ Probleme 0.py
@@ -4,5 +4,18 @@ Created on Wed Apr  2 13:22:52 2025
 
 @author: Noreddine
 """
+import random
+Hmax = 10000
 
-pipi
+def crée_demandes(n,Hmax):
+    demandes = []
+    for i in range(n):
+        hdeb = random.randint(0, Hmax)
+        hfin = random.randint(hdeb, Hmax)
+        while hfin == hdeb:
+            hfin = random.randint(hdeb, Hmax)
+        demandes.append((hdeb,hfin))
+    return demandes
+
+crée_demandes(10, Hmax)
+    
