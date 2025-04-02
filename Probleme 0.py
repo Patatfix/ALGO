cb45f5f11e6d300aef70fc7a1c21ca4c9d19e0b4
BOTET Gaël <gaelbotet@gmail.com>
2025-04-02 14:17:23 +0200

    Update Probleme 0.py

--- Probleme 0.py
+++ Probleme 0.py
@@ -1,8 +1,23 @@
+import random
+Hmax = 10000
+
+def crée_demandes(n,Hmax):
+    demandes = []
+    for i in range(n):
+        hdeb = random.randint(0, Hmax)
+        hfin = random.randint(hdeb, Hmax)
+        while hfin == hdeb:
+            hfin = random.randint(hdeb, Hmax)
+        demandes.append((hdeb,hfin))
+    return demandes
+
+demandes = crée_demandes(10, Hmax)
+
 def tri_rdv(demandes, borne_de_tri = "début"): #trie les rdv par ordre d'heure de début et ajoute un indice pour retrouver l'ordre initial des rdv dans la variable demandes
     #bornes_de_tri peut etre "début" ou "fin"
     n = len(demandes)
     
-    if isinstance(demandes, tuple) == True: #verifie si il y a déja l'indice de tri ou non
+    if isinstance(demandes, tuple) == True: # verifie si il y a déja l'indice de tri ou non
         demandes_classées = [[demandes[i], i] for i in range(n)] #si non, on ajoute un indice pour pouvoir ranger a la fin dans l'ordre original
     else : 
         demandes_classées = demandes #si oui
@@ -11,5 +26,7 @@ def tri_rdv(demandes, borne_de_tri = "début"): #trie les rdv par ordre d'heure 
         demandes_classées = sorted(demandes_classées, key = lambda x: x[0][0]) #on trie par heure de début de rdv
     elif borne_de_tri == "fin": 
         demandes_classées = sorted(demandes_classées, key = lambda x: x[0][1]) #si on veut trier par heure de fin de rdv
+    elif bornes_de_tri == "longueur":
+        demandes_classées = sorted(demandes_classées, key = lambda x: abs(x[0][0] - x[0][1]) #si on veut trier par durée du rdv
 
     return (demandes_classées)
