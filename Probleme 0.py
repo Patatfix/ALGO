84a48d376d292265b984bbc1ff517600c03b5f4d
Adrien Candela <adrien.candela@gmail.com>
2025-04-07 14:39:16 +0200

    Seance 1

--- Probleme 0.py
+++ Probleme 0.py
@@ -6,6 +6,12 @@ def compatible(creneau_1, creneau_2):
         return False
     return True
 
+def compatible_planning(planning, creneau):
+    for cre in planning:
+        if not compatible(cre, creneau):
+            return False
+    return True
+
 def crée_demandes(n,Hmax):
     demandes = []
     for i in range(n):
@@ -17,3 +23,29 @@ def crée_demandes(n,Hmax):
     return demandes
 
 crée_demandes(10, Hmax)
+
+def tri_rdv(demandes, borne_de_tri = "début"): #trie les rdv par ordre d'heure de début et ajoute un indice pour retrouver l'ordre initial des rdv dans la variable demandes
+    #bornes_de_tri peut etre "début", "fin" ou "longueur"
+    n = len(demandes)
+    
+    if isinstance(demandes, tuple) == True: # verifie si il y a déja l'indice de tri ou non
+        demandes_classées = [[demandes[i], i] for i in range(n)] #si non, on ajoute un indice pour pouvoir ranger a la fin dans l'ordre original
+    else : 
+        demandes_classées = demandes #si oui
+        
+    if borne_de_tri == "début" : 
+        demandes_classées = sorted(demandes_classées, key = lambda x: x[0][0]) #on trie par heure de début de rdv
+    elif borne_de_tri == "fin": 
+        demandes_classées = sorted(demandes_classées, key = lambda x: x[0][1]) #si on veut trier par heure de fin de rdv
+    elif bornes_de_tri == "longueur":
+        demandes_classées = sorted(demandes_classées, key = lambda x: abs(x[0][0] - x[0][1]) #si on veut trier par durée du rdv
+    else : 
+        print("aucun tri effectué, borne_de_tri invalide")
+
+    return (demandes_classées)
+
+#def optim_planning(demandes , nbr_jour = 0, i = 0):
+ #   if len(demandes) == 0:
+  #      return 
+   # for j in range(i,len(demandes)):
+    #    if demandes[]
