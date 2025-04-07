af7740ac7595710978434c9ddd8042947da0eb1c
Adrien Candela <adrien.candela@gmail.com>
2025-04-07 14:34:59 +0200

    compatible

--- Probleme 0.py
+++ Probleme 0.py
@@ -1,6 +1,11 @@
 import random
 Hmax = 10000
 
+def compatible(creneau_1, creneau_2):
+    if (creneau_1[0] >= creneau_2[0] and creneau_1[0] <= creneau_2[1]) or (creneau_1[1] >= creneau_2[0] and creneau_1[1] <= creneau_2[1]):
+        return False
+    return True
+
 def crée_demandes(n,Hmax):
     demandes = []
     for i in range(n):
